b085b4dca2f777bed8f18f8daf3b5c2f8d1f1444
yashingle1307 <yashingle1307@gmail.com>
2026-02-24 12:19:06 +0530

    Moved MongoDB URI to .env and secured config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(
-            "mongodb+srv://YashIngle:Yash0508@cluster0.zownxeg.mongodb.net/MediCarePlus?retryWrites=true&w=majority"
-        );
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DB Connected");
     } catch (error) {
         console.log("DB Error:", error.message);
     }
 };
-
-
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import dotenv from "dotenv";
 import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from './config/db.js';
 import doctorRouter from './routes/doctorRoutes.js';
@@ -34,7 +35,7 @@ app.use(cors({
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+dotenv.config();
 // DB
 connectDB();
 
